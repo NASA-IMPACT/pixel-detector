81a7e4babba39c277c44c6061c25910514ad1536
Ashish Acharya <ashish.acharya14@gmail.com>
2019-01-09 13:39:19 -0600

    code cleanup: predict.py

--- code/predict.py
+++ code/predict.py
@@ -1,20 +1,23 @@
-import matplotlib
-matplotlib.use('Agg')
-from keras.layers import Input, Dense
-from keras.models import load_model
-import numpy as np
-from PIL import Image
 import argparse
-import os
-import numpy as np
-import json
-from preprocessing import create_tiles, convert_pixels_to_groups, unison_shuffled_copies
-from tif_utils import create_array_from_nc,band_list
-from config import BANDS_LIST
-
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+import os
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
+from config import BANDS_LIST
+from keras.models import load_model
+from PIL import Image
+from preprocessing import (
+    convert_pixels_to_groups,
+)
+from tif_utils import (
+    create_array_from_nc,
+    band_list
+)
+
+matplotlib.use('Agg')
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 argparser = argparse.ArgumentParser(
@@ -47,29 +50,28 @@ argparser.add_argument(
     help='bitmap to visualize output')
 
 
-
 def _main_(args):
     print(args)
     model = load_model(args.model)
     n_size = args.neighborhoodSize
     ncFiles_path = args.input
-    X_mtx,_ = create_array_from_nc(band_list(ncFiles_path,BANDS_LIST,time=''),fname = '')
+    X_mtx, _ = create_array_from_nc(band_list(ncFiles_path, BANDS_LIST, time=''), fname='')
     model.summary()
     print('predicting...')
     y_out = model.predict(
         convert_pixels_to_groups(X_mtx),
-        batch_size = 10000)
-    y_mat = np.asarray(y_out*255.0,dtype = 'uint8').reshape(X_mtx[:,:,0].shape)
+        batch_size=10000)
+    y_mat = np.asarray(y_out * 255.0, dtype='uint8').reshape(X_mtx[:, :, 0].shape)
     Image.fromarray(y_mat).save('test.bmp')
     if not args.visualize == '':
 
         fig = plt.figure(figsize=(12, 10), dpi=100)
-        ax = fig.add_axes([0,0,1,1])
+        ax = fig.add_axes([0, 0, 1, 1])
         plt.axis('off')
-        ax.imshow(X_mtx[:,:,0])
+        ax.imshow(X_mtx[:, :, 0])
         ax.imshow(y_mat, alpha=0.5, cmap='gray')
-        #temp_y_out = y_out
-        #ax.imshow(temp_y_out, alpha=0.3, cmap='jet')
+        # temp_y_out = y_out
+        # ax.imshow(temp_y_out, alpha=0.3, cmap='jet')
         plt.savefig('test.png')
         plt.close()
 

3cacb22a38f582859d464975604dba793ffa447c
Muthukumaran Ramasubramanian <mramasub@dsig1.nsstc.uah.edu>
2019-05-16 10:55:35 -0500

    refactor code

--- code/evaluate.py
+++ code/evaluate.py
@@ -1,33 +1,29 @@
-# @author Muthukumaran R.
-
-import matplotlib
-matplotlib.use('Agg')
-
-import cv2
-import json
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import rasterio
-
-
-from keras.models import load_model
-from PIL import Image
-from sklearn.metrics import confusion_matrix
+# -*- coding: utf-8 -*-
+# @Author: Muthukumaran R.
+# @Date:   2019-04-02 12:13:51
+# @Last Modified by:   Muthukumaran R.
+# @Last Modified time: 2019-05-16 10:52:03
 
 from config import (
     PREDICT_THRESHOLD,
     OUTPUT_DIR,
-    BAND_1_FILENAME,
-    EVAL_DISP_STR
 )
-from preprocessing import (
-    get_arrays_from_json,
-    convert_bmp_to_shp,
-    get_bitmap_from_shp,
-    IOU_score
+from data_helper import (
+    get_data,
 )
+
+import matplotlib
+matplotlib.use('Agg')
+
+from keras.models import load_model
+from PIL import Image
+
+import json
+import numpy as np
+import os
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
 
 class Evaluate:
 
@@ -60,189 +56,127 @@ class Evaluate:
         self.predict_thres = PREDICT_THRESHOLD
         self.output_path = OUTPUT_DIR
         self.eval_img_path = 'evaluation_plot.png'
-        self.band_img_path = 'band1.png'
+        self.band1_img_path = 'band1.png'
+        self.band2_img_path = 'band2.png'
+        self.band3_img_path = 'band3.png'
         self.pix_bmp_path = 'pred_pix_bitmap.bmp'
-        self.shp_bmp_path = 'pred_shp_bitmap.bmp'
         self.t_bmp_path = 'true_bitmap.bmp'
-        self.shp_path = 'shapefile.shp'
-        self.true_overlay = 'true_overlay.png'
-        #self.model_holder = MODELS[self.config['type']] (self.config)
 
     def evaluate(self):
         """
-        desc:   produce images containing predictions along with their scores
-                scores produced: Accuracy, precision, recall, IOU score.
+        Description: evaluation workflow
         """
-
         print('looking for model in', str(self.config['model_path']))
         self.model = load_model(str(self.config['model_path']))
 
-        x_list, y_list, cache_list = get_arrays_from_json(self.config['eval_json'],
-                                                          self.config['num_neighbor']
-                                                          )
-        for x, y, cache_dir in zip(x_list, y_list, cache_list):
-            # iterate through each x,y pair
+        x_list, y_list, b_list, transforms = get_data(
+            str(self.config['eval_json']),
+            self.config['num_neighbor']
+        )
 
-            output_folder = os.path.join(self.output_path,
-                                         os.path.basename(os.path.normpath(cache_dir)))
+        for _id, (x, y, b_list, transform) in \
+                enumerate(zip(x_list, y_list, b_list, transforms)):
+
+            # make output folder
+            output_folder = os.path.join(self.output_path, str(_id))
             if not os.path.exists(output_folder):
                 os.makedirs(output_folder)
 
-            self.make_predict_bitmaps(x, output_folder, cache_dir)
+            # make prediction
+            y_pred = self.model.predict(
+                x, batch_size=self.config['batch_size'])
+            y_pred = y
+            y_pred = (y_pred > PREDICT_THRESHOLD) * 1.0
+            y_mat = self.reshape_array_to_image(
+                (y_pred) * 255.0,
+                b_list.shape[0],
+                b_list.shape[1])
+            y_true = self.reshape_array_to_image(
+                y * 255.0, b_list.shape[0], b_list.shape[1])
 
-            # save BAND 1 raster from cache for evaluation purposes
-            b1_rast = Image.open(os.path.join(
-                cache_dir, BAND_1_FILENAME)).convert('RGBA')
-            b1_rast.save(os.path.join(output_folder, self.band_img_path))
+            # save predicted images
+            self.save_image(y_mat, os.path.join(
+                output_folder, self.pix_bmp_path))
+            self.save_image(y_true, os.path.join(
+                output_folder, self.t_bmp_path))
 
-            self.plot_evaluated_image(output_folder)
-
-    def make_predict_bitmaps(self, x, output_folder, cache_dir):
-        """
-        Desc:   Get the predictions for x and store the respective bitmaps
-                in output directory, along with true bitmaps
-
-        input:  cache_dir       :  directory where the cache for the ncfile is
-                                    stored
-                x               :  input to the keras model
-                output_folder   :  output folder to store predictions
-
-        output: y: predicted pixels formatted as Image array.
-
-        """
-
-        y_true = Image.open(os.path.join(
-            cache_dir,
-            'bitmap_WGS84.bmp')
-        ).convert('L')
-        y_true_arr = np.asarray(y_true)
-        y_pred = self.model.predict(x, batch_size=self.config['batch_size'])
-
-        y_pred_bin = np.where(y_pred > self.predict_thres,
-                              1, 0)  # thresholding outputs
-        # y_pred_bin  *= y_pred
-        # y_pred_bin = y_pred
-
-        y_mat = self.reshape_array_to_image(
-            y_pred_bin * 255.0, y_true.size[1], y_true.size[0])
-
-        # store the true and predicted bitmaps in output_dir
-        self.save_image(y_mat, os.path.join(output_folder, self.pix_bmp_path))
-
-        # create shapefile output and bitmap from predicted shapefile
-        with rasterio.open(os.path.join(cache_dir, BAND_1_FILENAME)) as rasterio_obj:
-
-            convert_bmp_to_shp(
-                Image.open(
-                    os.path.join(output_folder, self.pix_bmp_path)
-                ).convert('L'),
-                rasterio_obj.transform,
-                os.path.join(output_folder, self.shp_path)
-            )
-            y_shp = get_bitmap_from_shp(
-                os.path.join(output_folder, self.shp_path),
-                rasterio_obj,
-                os.path.join(output_folder, self.shp_bmp_path),
-            )
-
-        self.save_image(y_true_arr, os.path.join(
-            output_folder, self.t_bmp_path))
+            # plot images and predictions
+            self.plot_rgb(b_list[:, :, 1],
+                          b_list[:, :, 2],
+                          b_list[:, :, 0],
+                          y_true,
+                          y_mat,
+                          output_folder).savefig(
+                os.path.join(output_folder, self.eval_img_path))
 
     def reshape_array_to_image(self, dim1_array, x_shape, y_shape):
         """
-        desc    : reshape given 1D array to a 2D array of given x,y Dimensions
+        desc: reshape given 1D array to a 2D array of
+                given x,y Dimensions
         """
-        return np.asarray(dim1_array, dtype='uint8').reshape((x_shape, y_shape), order='C')
+        return np.asarray(dim1_array, dtype='uint8').reshape(
+            (x_shape, y_shape), order='C')
+
+    def plot_rgb(self, RL1, GL1, BL1, y_true, y_pred, output_folder):
+        """Summary
+
+        Args:
+            RL1 (TYPE): Description
+            GL1 (TYPE): Description
+            BL1 (TYPE): Description
+            y_true (numpy array): true feature
+            y_pred (numpy array): predicted feature
+            output_folder (string): folder to store plots
+
+        Returns:
+            TYPE: Description
+
+        """
+        print('b_list[1] shape', RL1)
+
+        # normalization to true RGB
+        GL1_true = 0.45 * ((RL1 / 25.5)**2 / 100) + 0.1 * \
+            ((GL1 / 25.5)**2 / 100) + 0.45 * ((BL1 / 25.5)**2 / 100)
+        GL1_true = np.maximum(GL1_true, 0)
+        GL1_true = np.minimum(GL1_true, 1)
+        RGBL1_veggie = np.dstack(
+            [((RL1 / 25.5) ** 2 / 100),
+             ((GL1 / 25.5) ** 2 / 100),
+             ((BL1 / 25.5) ** 2 / 100)])
+
+        # plotting RGB
+        fig, axes = plt.subplots(2, 3, figsize=(16, 8), dpi=250)
+        axes[0, 0].imshow(RL1, cmap='Reds', vmax=255, vmin=0)
+        axes[0, 0].set_title('Red', fontweight='semibold')
+        axes[0, 0].axis('off')
+        axes[0, 1].imshow(GL1, cmap='Greens', vmax=255, vmin=0)
+        axes[0, 1].set_title('Veggie', fontweight='semibold')
+        axes[0, 1].axis('off')
+        axes[0, 2].imshow(BL1, cmap='Blues', vmax=255, vmin=0)
+        axes[0, 2].set_title('Blue', fontweight='semibold')
+        axes[0, 2].axis('off')
+        plt.subplots_adjust(wspace=.02)
+        axes[1, 0].imshow(RGBL1_veggie)
+        axes[1, 0].axis('off')
+        axes[1, 1].imshow(RGBL1_veggie)
+
+        self.save_image(RL1, os.path.join(output_folder, 'r.bmp'))
+        self.save_image(GL1, os.path.join(output_folder, 'g.bmp'))
+        self.save_image(BL1, os.path.join(output_folder, 'b.bmp'))
+        axes[1, 1].imshow(y_true, alpha=0.15,)
+        axes[1, 1].axis('off')
+
+        axes[1, 2].imshow(y_pred)
+        axes[1, 2].imshow(y_true, alpha=0.15,)
+        axes[1, 2].axis('off')
+        return plt
 
     def save_image(self, img_array, loc):
         """
         Desc    : save given 'img_array' as image in the given 'loc' location
         """
-       Image.fromarray(img_array).save(loc)
+        Image.fromarray(img_array).convert('L').save(loc)
 
-    def plot_evaluated_image(self, output_folder):
-        """
-        Desc:   Plot the Evaluation Image with appropriate metrics
-                using matplotlib library
-                metrics used: Accuracy, precision, recall, IOU score.
-
-        input:  output_folder: folder where the images are to be stored.
-        """
-        # read true and predict bitmaps
-        y_true = mpimg.imread(os.path.join(output_folder, self.t_bmp_path))
-        y_shp = mpimg.imread(os.path.join(output_folder, self.shp_bmp_path))
-        y_pred = mpimg.imread(os.path.join(output_folder, self.pix_bmp_path))
-        b1_img = mpimg.imread(os.path.join(output_folder, self.band_img_path))
-
-        # create BAND_1 AND predicted bitmap subplots
-        plt.subplot(2, 1, 1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.imshow(b1_img)
-        plt.xlabel('GOES Band 1')
-        plt.subplot(2, 1, 2)
-        plt.imshow(y_true, alpha=1.0, cmap='plasma')
-        plt.imshow(y_shp, alpha=0.5, cmap='PuBu', vmin=125, vmax=255)
-        plt.imshow(y_pred, alpha=0.5, cmap='PuBuGn', vmin=125, vmax=255)
-
-        # add confusion matrix to the plot
-        a, r, p = self.conf_mtx(y_true, y_pred)
-        sa, sr, sp = self.conf_mtx(y_true, y_shp)
-
-        # add IOU score to the plot
-        iou = IOU_score(Image.open(os.path.join(output_folder, self.shp_bmp_path), mode='r'),
-                        Image.open(os.path.join(output_folder, self.t_bmp_path), mode='r'))
-
-        disp_str = EVAL_DISP_STR.format(a, r, p, iou)
-        plt.xlabel(disp_str)
-        # plt.axis('off')
-        plt.xticks([])
-        plt.yticks([])
-        plt.tight_layout()
-        plt.savefig(os.path.join(output_folder, self.eval_img_path),
-                    bbox_inches='tight')
-
-        plt.figure()
-        plt.imshow(b1_img)
-        plt.imshow(y_true, alpha=0.20, cmap='copper', vmin=0, vmax=255)
-        plt.xlabel('True Shapefile Overlaid on BAND 1')
-        plt.xticks([])
-        plt.yticks([])
-        plt.savefig(os.path.join(output_folder, self.true_overlay),
-                    bbox_inches='tight')
-        plt.close()
-
-    def conf_mtx(self, y_true, y_pred):
-        """
-        desc:   Give out A,R,P scores for given true,predict
-        Input:  y_true:     True Pixels
-                y_predict:  Predicted pixels
-        Output: acc         :
-                recall      :
-                precision   :
-        """
-        cm = confusion_matrix(
-            y_true.flatten() > self.predict_thres * 255.0,
-            y_pred.flatten() > self.predict_thres * 255.0
-        )
-
-        overall_true = float(cm[0][0] + cm[1][1])
-        total = float(cm[0][0] + cm[0][1] + cm[1][1] + cm[1][0])
-        true_positive = float(cm[1][1])
-        actual_positive = float(cm[1][0] + cm[1][1])
-        predicted_positive = float(cm[0][1] + cm[1][1])
-
-        acc = self.safe_div(overall_true, total)
-        recall = self.safe_div(true_positive, actual_positive)
-        precision = self.safe_div(true_positive, predicted_positive)
-
-        return acc, recall, precision
-
-    def safe_div(self, x, y):
-        if y == 0:
-            return 0
-
-        return x / y
 
 if __name__ == '__main__':
 

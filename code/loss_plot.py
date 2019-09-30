42b7891ecfc7579c538041a86382c6c55c5c4638
Muthukumaran Ramasubramanian <mramasub@dsig1.nsstc.uah.edu>
2019-09-30 16:17:23 -0500

    loss_plot.py refactor

--- code/loss_plot.py
+++ code/loss_plot.py
@@ -2,7 +2,7 @@
 # @Author: Muthukumaran R.
 # @Date:   2019-07-23 10:56:37
 # @Last Modified by:   Muthukumaran R.
-# @Last Modified time: 2019-09-17 15:28:29
+# @Last Modified time: 2019-09-30 16:12:51
 
 import matplotlib
 matplotlib.use('agg')
@@ -33,12 +33,13 @@ class TrainingPlot(keras.callbacks.Callback):
         self.val_losses.append(logs.get('val_loss'))
         self.val_acc.append(logs.get('val_acc'))
 
-        num_epochs = np.arange(0, len(self.losses))
+        self.num_epochs = np.arange(0, len(self.losses))
 
+    def loss_plot(self, epoch):
         plt.style.use("seaborn")
         plt.figure()
-        plt.plot(num_epochs, self.losses, label = "train_loss")
-        plt.plot(num_epochs, self.val_losses, label = "val_loss")
+        plt.plot(self.num_epochs, self.losses, label="train_loss")
+        plt.plot(self.num_epochs, self.val_losses, label="val_loss")
         plt.title("Training and Validation Loss".format(epoch))
         plt.xlabel("Epoch #")
         plt.ylabel("Loss")
@@ -47,8 +48,8 @@ class TrainingPlot(keras.callbacks.Callback):
         plt.close()
 
         plt.figure()
-        plt.plot(num_epochs, self.acc, label = "train_acc")
-        plt.plot(num_epochs, self.val_acc, label = "val_acc")
+        plt.plot(self.num_epochs, self.acc, label="train_acc")
+        plt.plot(self.num_epochs, self.val_acc, label="val_acc")
         plt.title("Training and Validation Accuracy".format(epoch))
         plt.xlabel("Epoch #")
         plt.ylabel("Accuracy")

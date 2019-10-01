ceb4466b2d19d16295e9ac2edb50fee46e771a32
Muthukumaran Ramasubramanian <mramasub@dsig1.nsstc.uah.edu>
2019-10-01 10:15:59 -0500

    minor refactors to evaluate/batch_evaluate

--- code/batch_evaluater.py
+++ code/batch_evaluater.py
@@ -40,18 +40,17 @@ class MidpointNormalize(Normalize):
 
 class BatchEvaluate(Evaluate):
 
-    def __init__(self, config, step_num=3, batch_size=50000):
+    def __init__(self, config, step_num=3):
         """init for Evaluate class
         """
         self.batch_size = config['batch_size']
         self.num_n = config['num_neighbor']
         self.model_path = config['model_path']
-        self.val_dir = config['val_dir']
+        self.val_dir = config['val_input_dir']
         self.save_dir = config['val_output_dir']
 
         self.model = load_model(self.model_path)
         print(self.model.summary())
-
         path_list = glob(self.val_dir + '/*.tif')
 
         paths_lists = [path_list[x:x + step_num] for x in range(
@@ -61,3 +60,9 @@ class BatchEvaluate(Evaluate):
                                              neighbour_pixels=self.num_n)
             self.dataset.iterate()
             self.evaluate()
+
+
+if __name__ == '__main__':
+    import json
+    config = json.load(open('config.json'))
+    ev = BatchEvaluate(config)

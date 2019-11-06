943ed403c0a63dd61a0ebe8c888c17f697e208b7
Carson Davis <CarsonReidDavis@gmail.com>
2019-11-06 13:31:13 -0600

    adds input generator and implements fstrings

--- code/train_combinator.py
+++ code/train_combinator.py
@@ -10,6 +10,46 @@ experiment = {'drop_1': [1, 2, 3, 4, 5],
               'drop_56': [0, 1, 2, 3],
               'baseline': [0, 1, 2, 3, 4, 5]}
 
+
+def experiment_input_generator(bands, method='just', baseline=[0, 1, 2, 3, 4, 5]):
+    """Generates a dictionary containing the experiment name and band list
+
+    Args:
+        bands ([ints]): List of integers representing bands to include or exclude
+        method (str, optional): Use 'just' to only use bands included in bands, or 'drop' to use
+            the baseline with the bands excluded. Defaults to 'just'.
+        baseline ([ints], optional): List of integers representing bands in the baseline. 
+            Defaults to [0,1,2,3,4,5].
+
+    Returns:
+        dict: {experiment_name: band_list}
+    """
+
+    valid_methods = ['just', 'drop']
+
+    if method not in valid_methods:
+        raise ValueError("method arg must be 'just' or 'drop'")
+
+    if not (type(bands) is list and all([type(band) is int for band in bands])):
+        raise ValueError("bands must be a list of integers")
+
+    if not (type(baseline) is list and all([type(band) is int for band in baseline])):
+        raise ValueError("baseline must be a list of integers")
+
+    if not all([band in baseline for band in bands]):
+        raise ValueError("all bands must be contained in baseline")
+
+    name = f"{method}_{''.join([str(band) for band in bands])}"
+
+    if method == 'drop':
+        final_bands = [band for band in baseline if band not in bands]
+
+    elif method == 'just':
+        final_bands = bands
+
+    return {'experiment_name': name, 'band_list': final_bands}
+
+
 with open("config.json", 'r') as file:
     config = json.load(file)
 
@@ -17,5 +57,5 @@ model_path = config["model_path"].replace('.h5', '')
 
 for title, bands in experiment:
     config["bands"] = bands
-    config["model_path"] = model_path+"_"+title+".h5"
+    config["model_path"] = f'{model_path}_{title}.h5'
     Trainer(config).train()

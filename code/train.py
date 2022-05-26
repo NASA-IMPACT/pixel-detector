2fa578369f4810930163ddbd3d021b84f4dc7f5b
xhagrg <grg.iksha@hotmail.com>
2022-05-26 11:37:40 -0500

    Use tensorflow mirrored strategy.

--- code/train.py
+++ code/train.py
@@ -21,14 +21,27 @@ class Trainer:
         """
 
         self.config = config
-        self.model_holder = MODELS[self.config['type']](self.config)
+        import tensorflow as tf
+        from tensorflow import keras
+        from tensorflow.keras import layers
+        # model building
+        tf.keras.backend.clear_session()  # For easy reset of notebook state.
+
+        print('Trying to resolve cluster')
+        cluster, my_job_name, my_task_index = tf_config_from_slurm(ps_number=1)
+        slurm_resolver = SlurmClusterResolver()
+        print(slurm_resolver.cluster_spec())
+        print('Resolved cluster')
+        mirrored_strategy = tf.distribute.MultiWorkerMirroredStrategy(cluster_resolver=slurm_resolver)
+        print('Number of replicas:', mirrored_strategy.num_replicas_in_sync)
+        with mirrored_strategy.scope():
+            self.model_holder = MODELS[self.config['type']](self.config)
 
     def train(self):
         """
         Alias to model holder train method
         """
-
-        self.model_holder.train()
+            self.model_holder.train()
 
     def load(self):
         """
